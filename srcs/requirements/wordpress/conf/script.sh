ff1013b9ddca72e918284ddb516ac735f95bb030
nonomex <nonomex175@gmail.com>
2025-02-25 17:21:16 +0000

    fds

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -14,17 +14,35 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
+cp /usr/local/bin/wp /var/www/html/wordpress/wp-cli.phar
+
 chown -R www-data:www-data /var/www/html
 
 chmod -R 755 /var/www/html
 
-rm /var/www/html/wordpress/wp-config.php
+mv /wp-config.php /var/www/html/wordpress/wp-config.php
 
-wp config create --dbname="mysql" --dbuser="aammirat" --dbpass="passwourd" --dbhost="mariadb:3306" --allow-root --path=/var/www/html/wordpress
+#wp config create --dbname="mysql" --dbuser="aammirat" --dbpass="passwourd" --dbhost="mariadb:3306" --allow-root --path=/var/www/html/wordpress
+
+#echo '<VirtualHost *:9000>
+ #   ServerName aammirat.42.fr
+  #  DocumentRoot /var/www/html/wordpress
+   # <Directory /var/www/html/wordpress>
+    #    AllowOverride All
+     #   Require all granted
+    #</Directory>
+    #ErrorLog ${APACHE_LOG_DIR}/error.log
+    #CustomLog ${APACHE_LOG_DIR}/access.log combined
+#</VirtualHost>' > /etc/apache2/sites-available/000-default.conf
+
+#echo 'ServerName aammirat.42.fr' >> /etc/apache2/apache2.conf
 
 echo install
 
-wp core install --url="aammirat.42.fr" --title="inception" --admin_user="aammirat" --admin_password="passwourd" --admin_email="thecreator@donthaveemail.com" --allow-root --path=/var/www/html/wordpress
+
+#echo apache launched, nw installgin
+
+wp core install --url="https://aammirat.42.fr" --title="inception" --admin_user="aammirat" --admin_password="passwourd" --admin_email="thecreator@donthaveemail.com" --allow-root --path=/var/www/html/wordpress
 
 echo heyogf
 
@@ -34,8 +52,7 @@ done
 
 echo service launch 
 
-service apache2 start
+wp user create ${USER_NAME} ${USER_MAIL} --user_pass=${USER_PASSWORD} --path=/var/www/html/wordpress --allow-root
 
-wp user create ${USER_NAME} ${USER_MAIL} --user_pass=${USER_PASSWORD} --path=/var/www/html/wordpress -allow-root
-
-exec "$@"
+php7.3-fpm
+#service apache2 start
