77d78f1e3ef100cd73ebc97ff7041fd606f5b701
nonomex <nonomex175@gmail.com>
2025-02-26 13:55:00 +0000

    YEY

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -22,25 +22,7 @@ chmod -R 755 /var/www/html
 
 mv /wp-config.php /var/www/html/wordpress/wp-config.php
 
-#wp config create --dbname="mysql" --dbuser="aammirat" --dbpass="passwourd" --dbhost="mariadb:3306" --allow-root --path=/var/www/html/wordpress
-
-#echo '<VirtualHost *:9000>
- #   ServerName aammirat.42.fr
-  #  DocumentRoot /var/www/html/wordpress
-   # <Directory /var/www/html/wordpress>
-    #    AllowOverride All
-     #   Require all granted
-    #</Directory>
-    #ErrorLog ${APACHE_LOG_DIR}/error.log
-    #CustomLog ${APACHE_LOG_DIR}/access.log combined
-#</VirtualHost>' > /etc/apache2/sites-available/000-default.conf
-
-#echo 'ServerName aammirat.42.fr' >> /etc/apache2/apache2.conf
-
 echo install
-
-
-#echo apache launched, nw installgin
 
 wp core install --url="https://aammirat.42.fr" --title="inception" --admin_user="aammirat" --admin_password="passwourd" --admin_email="thecreator@donthaveemail.com" --allow-root --path=/var/www/html/wordpress
 
@@ -54,5 +36,7 @@ echo service launch
 
 wp user create ${USER_NAME} ${USER_MAIL} --user_pass=${USER_PASSWORD} --path=/var/www/html/wordpress --allow-root
 
-php7.3-fpm
-#service apache2 start
+mkdir /run/php
+chmod 755 /run/php
+
+/usr/sbin/php-fpm7.3 -F 
